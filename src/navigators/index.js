33d838add4ee9711897f705da8e2fb1a32a90f19
Daniel Dupont <danieldupont.contact@gmail.com>
2020-03-19 10:52:46 +0100

    master

--- src/navigators/index.js
+++ src/navigators/index.js
@@ -1,11 +1,13 @@
 import {createAppContainer} from 'react-navigation';
 import {createBottomTabNavigator} from 'react-navigation-tabs';
 import {HomeStack} from './HomeStack';
+import {SelectRateSwitchNavigator} from './SelectRateSwiteNavigator';
 import { SelectRateStack } from './SelectRateStack';
 
 const TabBarNavigator = createBottomTabNavigator({
   Home: HomeStack,
-  SelectRateSwitch: SelectRateStack,
+  // SelectRateSwitch: SelectRateStack,
+  SelectRateSwitch: SelectRateSwitchNavigator,
   // Settings: Screens.Settings,
 });
 
